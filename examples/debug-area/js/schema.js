f877e5dc0ef9d19330fdd1e809e20ddaa4d6969d
andru255 <andru255@gmail.com>
2014-06-07 02:09:33 -0500

    All: Append first part of the great test fire

--- examples/debug-area/js/schema.js
+++ examples/debug-area/js/schema.js
@@ -14,7 +14,8 @@ var schema = {
                     },
                     'actions':{
                         'dashboard': function(){
-                            alert('corriendo action dashboard del controller aviso');
+                            yOSON.AppCore.runModule("demoA");
+                            yOSON.AppCore.runModule("demoB");
                         },
                         'byDefault':function(){
                             //alert("si no existe un action, este action corre por defecto");
@@ -31,32 +32,3 @@ var schema = {
         }
     }
 };
-var objLoader = new yOSON.Loader(schema);
-//
-var querys = {
-    "nothing":"",
-    "onlyModule":"usuario",
-    "moduleWithController":{
-        "module": "usuario",
-        "controller": "aviso"
-    },
-    "moduleControllerAction":{
-        "module": "usuario",
-        "controller": "aviso",
-        "action": "dashboard"
-    }
-}
-//querys.nothing
-var query = querys.nothing;
-objLoader.init(querys.nothing);
-//querys.onlymodule
-alert("querys.onlyModule");
-query = querys.onlyModule;
-objLoader.init(query);
-alert("modulewithcontroller");
-query = querys.moduleWithController;
-objLoader.init(query.module, query.controller);
-//querys.modulewithcontrollerAction
-alert("moduleControllerAction");
-query = querys.moduleControllerAction;
-objLoader.init(query.module, query.controller, query.action);
