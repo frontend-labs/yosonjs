ecb52c5654dd7064e5e72c5088f4821ac937a8ee
Andrés Muñoz <andres.munoz@orbis.pem>
2014-06-13 20:10:58 -0700

    yOSON: Append first scripts of TDD

---
+++ src/core.js
@@ -0,0 +1,68 @@
+define([
+    "yoson",
+    "comps/dependency",
+    "comps/dependency-manager",
+    "comps/modular",
+    "comps/comunicator",
+], function(yOSON){
+    yOSON.AppCore = (function(){
+        var objModular = new yOSON.Modular(),
+            dependencyManager = new yOSON.DependencyManager(),
+            objComunicator = new yOSON.Comunicator(),
+            dependenceByModule = {};
+
+        //setting the main methods in the bridge of an module
+        objModular.addMethodToBrigde('events', function(eventNames, functionSelfEvent, instanceOrigin){
+            objComunicator.subscribe(eventNames, functionSelfEvent, instanceOrigin);
+        });
+
+        objModular.addMethodToBrigde('trigger', function(eventName, argumentsOfEvent){
+            var eventsWaiting = {};
+
+            console.log('corriendo evento', eventName);
+            objModular.allModulesRunning(function(){
+                eventsWaiting[eventName] = argumentsOfEvent;
+            }, function(){
+                //if have events waiting
+                for(var eventsForTrigger in eventsWaiting){
+                    objComunicator.publish(eventsForTrigger , eventsWaiting[eventsForTrigger]);
+                }
+                objComunicator.publish(eventName, argumentsOfEvent);
+            });
+        });
+
+        //managing the dependences
+        var setDependencesByModule = function(moduleName, dependencesOfModule){
+            dependenceByModule[moduleName] = dependencesOfModule;
+        },
+        getDependencesByModule = function(moduleName){
+            var dependencesToReturn = [];
+            if(dependenceByModule[moduleName]){
+                dependencesToReturn = dependenceByModule[moduleName];
+            }
+            return dependencesToReturn;
+        };
+
+        return {
+            addModule: function(moduleName, moduleDefinition, dependences){
+                setDependencesByModule(moduleName, dependences);
+                objModular.addModule(moduleName, moduleDefinition);
+            },
+            runModule: function(moduleName, optionalParameter){
+                var dependencesToLoad = getDependencesByModule(moduleName);
+                objModular.setStatusModule(moduleName, "start");
+                dependencyManager.ready(dependencesToLoad,function(){
+                    objModular.runModule(moduleName, optionalParameter);
+                });
+            },
+            setStaticHost: function(hostName){
+                dependencyManager.setStaticHost(hostName);
+            },
+            setVersionUrl: function(versionCode){
+                dependencyManager.setVersionUrl(versionCode);
+            }
+        };
+    })();
+
+    return yOSON.AppCore;
+});
