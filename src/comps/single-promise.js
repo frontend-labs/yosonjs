e76145b07188f245d98dd326c24eb9784a10b93b
Andrés Muñoz <andres.munoz@orbis.pem>
2014-09-01 16:31:21 -0500

    yOSON Single Promise: Applying hint

---
+++ src/comps/single-promise.js
@@ -0,0 +1,42 @@
+define([
+    "yoson"
+], function(yOSON){
+
+    var SinglePromise = function(){
+        this.callbacks = {
+            succeededs:[],
+            faileds:[]
+        };
+    };
+
+    SinglePromise.prototype.eachCallBackList = function(callbackList, onEveryCallback){
+        for(var indexCallback = 0; indexCallback < callbackList.length; indexCallback++){
+            onEveryCallback.call(this, callbackList[indexCallback]);
+        }
+    };
+
+    SinglePromise.prototype.done = function(){
+        this.eachCallBackList(this.callbacks.succeededs, function(callbackRegistered){
+            callbackRegistered.call(this);
+        });
+    };
+
+    //when all tasks its success
+    SinglePromise.prototype.then = function(whenItsDone, whenIsFailed){
+        this.callbacks.succeededs.push(whenItsDone);
+        if(typeof whenIsFailed === "function"){
+            this.callbacks.faileds.push(whenIsFailed);
+        }
+        return this;
+    };
+
+    //when the promise is broken
+    SinglePromise.prototype.fail = function(objError){
+        this.eachCallBackList(this.callbacks.faileds, function(callbackRegistered){
+            callbackRegistered.call(this, objError);
+        });
+    };
+
+    yOSON.SinglePromise = SinglePromise;
+    return SinglePromise;
+});
