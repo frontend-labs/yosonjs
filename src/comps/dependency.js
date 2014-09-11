8bcc8d51c4b46ec7e20815707d6b566b0c72aadb
Andrés Muñoz <andres.munoz@orbis.pem>
2014-09-11 12:52:02 -0500

    yOSON dependency Component: Remove support callback when script have an error in load under IE

--- src/comps/dependency.js
+++ src/comps/dependency.js
@@ -111,10 +111,8 @@ define([
         if(scriptElement.readyState){
             scriptElement.onreadystatechange = function(){
                 if(scriptElement.readyState=="loaded" || scriptElement.readyState=="complete"){
-                    that.onReadyRequest();
                     scriptElement.onreadystatechange=null;
-                } else {
-                    that.onErrorRequest();
+                    that.onReadyRequest();
                 }
             };
         } else {
