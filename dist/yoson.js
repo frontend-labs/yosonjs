d46203b4bc4a2a8cf7846e1262ee27698c5ef569
Andrés Muñoz <andres.munoz@orbis.pem>
2014-09-15 12:38:52 -0500

    yOSON Component SinglePromise: Append status of the promise

--- dist/yoson.js
+++ dist/yoson.js
@@ -27,6 +27,7 @@
             succeededs:[],
             faileds:[]
         };
+        this.status = "pending";
     };
 
     SinglePromise.prototype.eachCallBackList = function(callbackList, onEveryCallback){
@@ -36,6 +37,7 @@
     };
 
     SinglePromise.prototype.done = function(){
+        this.status = "done";
         this.eachCallBackList(this.callbacks.succeededs, function(callbackRegistered){
             callbackRegistered.call(this);
         });
@@ -43,15 +45,30 @@
 
     //when all tasks its success
     SinglePromise.prototype.then = function(whenItsDone, whenIsFailed){
-        this.callbacks.succeededs.push(whenItsDone);
-        if(typeof whenIsFailed === "function"){
-            this.callbacks.faileds.push(whenIsFailed);
-        }
+        var callbacks = this.callbacks;
+        var byStatus = {
+            "pending": function(){
+                callbacks.succeededs.push(whenItsDone);
+                if(typeof whenIsFailed === "function"){
+                    callbacks.faileds.push(whenIsFailed);
+                }
+            },
+            "done": function(){
+                if(typeof whenItsDone === "function"){
+                    whenItsDone.call(this);
+                }
+            },
+            "fail": function(){
+                whenIsFailed.call(this);
+            }
+        };
+        byStatus[this.status]();
         return this;
     };
 
     //when the promise is broken
     SinglePromise.prototype.fail = function(objError){
+        this.status = "fail";
         this.eachCallBackList(this.callbacks.faileds, function(callbackRegistered){
             callbackRegistered.call(this, objError);
         });
--- src/comps/single-promise.js
+++ src/comps/single-promise.js
@@ -7,6 +7,7 @@ define([
             succeededs:[],
             faileds:[]
         };
+        this.status = "pending";
     };
 
     SinglePromise.prototype.eachCallBackList = function(callbackList, onEveryCallback){
@@ -16,6 +17,7 @@ define([
     };
 
     SinglePromise.prototype.done = function(){
+        this.status = "done";
         this.eachCallBackList(this.callbacks.succeededs, function(callbackRegistered){
             callbackRegistered.call(this);
         });
@@ -23,15 +25,30 @@ define([
 
     //when all tasks its success
     SinglePromise.prototype.then = function(whenItsDone, whenIsFailed){
-        this.callbacks.succeededs.push(whenItsDone);
-        if(typeof whenIsFailed === "function"){
-            this.callbacks.faileds.push(whenIsFailed);
-        }
+        var callbacks = this.callbacks;
+        var byStatus = {
+            "pending": function(){
+                callbacks.succeededs.push(whenItsDone);
+                if(typeof whenIsFailed === "function"){
+                    callbacks.faileds.push(whenIsFailed);
+                }
+            },
+            "done": function(){
+                if(typeof whenItsDone === "function"){
+                    whenItsDone.call(this);
+                }
+            },
+            "fail": function(){
+                whenIsFailed.call(this);
+            }
+        };
+        byStatus[this.status]();
         return this;
     };
 
     //when the promise is broken
     SinglePromise.prototype.fail = function(objError){
+        this.status = "fail";
         this.eachCallBackList(this.callbacks.faileds, function(callbackRegistered){
             callbackRegistered.call(this, objError);
         });
